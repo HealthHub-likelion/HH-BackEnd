30764544a624b05749a2dc4b31516e2deea933ca
star-sil <kimse9811@naver.com>
2022-08-04 01:12:34 +0900

    로그인 기능 추가

--- HealthHub/accounts/urls.py
+++ HealthHub/accounts/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.urls import path, include
-from .views import MemberViewSet, MemberCheckViewSet
+from .views import MemberViewSet, MemberCheckViewSet, MemberSessionViewSet
 
 router = DefaultRouter()
 # 첫 번째 인자는 url의 prefix
@@ -18,8 +18,13 @@ member_check = MemberCheckViewSet.as_view({
     'post': 'check_member'
 })
 
+member_session = MemberSessionViewSet.as_view({
+    'post' : 'login'
+})
+
 
 urlpatterns =[
     path('', member),
-    path('member/',member_check)
+    path('member/',member_check),
+    path('member/login',member_session)
 ]
