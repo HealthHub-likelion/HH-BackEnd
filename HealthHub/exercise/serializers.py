590dc31fd3d87ffaae853421213d4665ca4da627
youngmin1006 <beanpole_1006@naver.com>
2022-08-04 01:25:36 +0900

    add fork, routine permission

--- HealthHub/exercise/serializers.py
+++ HealthHub/exercise/serializers.py
@@ -4,26 +4,36 @@ from rest_framework import serializers
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model=Exercise
-        fields= '__all__'
-
-class RoutineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Routine
-        fields= '__all__'
-        # fields= ("creatorName","routineName","count","isOpen")
-
-
-class RoutineExerciseSerializer(serializers.ModelSerializer):
-    # routines = RoutineSerializer()
-    # exercise = ExerciseSerializer()
-    class Meta:
-        model=RoutineExercise
-        # fields= ("routines","exercises")
-        fields= '__all__'
-    
+        # fields= '__all__'
+        fields = ('en_name','ko_name','part','id')
 
 
 class SetSerializer(serializers.ModelSerializer):
     class Meta:
         model=Set
         fields= '__all__'
+
+
+
+class RoutineExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=RoutineExercise
+        # fields=  ('id','routine_id')
+        fields= '__all__'
+
+
+class RoutineSerializer(serializers.ModelSerializer):
+    # re_connect_ex = RoutineExerciseSerializer(source='RoutineExercise')
+    # re_connect_ex =  serializers.RelatedField(many=True)
+    class Meta:
+        model=Routine
+        fields= '__all__'
+        # fields= ('id','creatorName','routineName','count','isOpen','member_id','RE_list')
+    
+    # def get_RE_list(self,obj):
+    #     routine_query = RoutineExercise.objects.filter(
+    #         # re_routine = obj.id
+    #         routine_id = obj.id
+    #     )   
+    #     serializer =  RoutineExerciseSerializer(routine_query,many=True)
+    #     return serializer.data
--- HealthHub/exercise/views.py
+++ HealthHub/exercise/views.py
@@ -1,24 +1,68 @@
 from django.shortcuts import render
 from .models import Exercise,Routine,RoutineExercise,Set
+from accounts.models import Member
 from .serializers import ExerciseSerializer,RoutineSerializer,RoutineExerciseSerializer,SetSerializer
-
+import json
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import generics, status
 
 
-class ExerciseViewSet(viewsets.ModelViewSet):
+class ymExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
-
-class RoutineViewSet(viewsets.ModelViewSet):
+class ymRoutineViewSet(viewsets.ModelViewSet):
     queryset = Routine.objects.all()
     serializer_class = RoutineSerializer
 
-class RoutineExerciseViewSet(viewsets.ModelViewSet):
+class ymRoutineDetailViewSet(viewsets.ModelViewSet):
+    queryset = Routine.objects.all()
+    serializer_class = RoutineSerializer
+
+    def partial_update(self,request,pk):
+        data = json.loads(request.body)
+        isopen = data['isOpen']
+        if isopen == "True" or isopen == "False":
+            routine = Routine.objects.get(id = pk)
+            routine.isOpen = data['isOpen']
+            routine.save()
+            return Response(True,status=status.HTTP_200_OK)
+        else:
+            return Response(False,status=status.HTTP_200_OK)
+
+class ymRoutineForkViewSet(viewsets.ModelViewSet):
+    queryset = Routine.objects.all()
+    serializer_class = RoutineSerializer
+
+    def update(self,request,pk):
+        # data = json.loads(request.header)
+        print(request.META.get('HTTP_TOKEN'))
+        member = Member.objects.get(token = request.META.get('HTTP_TOKEN'))
+        oriroutine = Routine.objects.get(id = pk)
+        newroutine = Routine.objects.create(
+            member_id = member,#본인 
+            creatorName = oriroutine.creatorName, #출처 닉네임
+            routineName = oriroutine.routineName,
+            count = 0,
+            isOpen = True
+        )
+        re_obj_list = RoutineExercise.objects.filter(routine_id = pk)
+        for re_obj in re_obj_list:
+            ex_obj = Exercise.objects.get(id = re_obj.exercise_id.id)
+
+            new_re_obj = RoutineExercise.objects.create(
+                routine_id = newroutine,#새로운 루틴_id
+                exercise_id = ex_obj#기존 운동_id
+            )
+        return Response(True,status=status.HTTP_200_OK)
+
+
+class ymRoutineExerciseViewSet(viewsets.ModelViewSet):
     queryset = RoutineExercise.objects.all()
     serializer_class = RoutineExerciseSerializer
 
-class SetViewSet(viewsets.ModelViewSet):
+class ymSetViewSet(viewsets.ModelViewSet):
     queryset = Set.objects.all()
     serializer_class = SetSerializer
