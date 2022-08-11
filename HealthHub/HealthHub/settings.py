2bbe5eb269c1aa9fd66b6e999d5e2ca59cd5ad2d
김성은 <70811575+star-sil@users.noreply.github.com>
2022-08-11 22:15:39 +0900

    Update settings.py

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from pickle import TRUE
+from .database import * 
 # import mysettings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
