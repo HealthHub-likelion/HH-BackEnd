7ec80b9b9735a6919c14be37f287499618a7bbfe
youngmin1006 <67896917+youngmin1006@users.noreply.github.com>
2022-08-11 22:17:40 +0900

    Update settings.py

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'HealthHub.wsgi.application'
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'DB_HOST',
-        'PORT': 'DB_PORT',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
