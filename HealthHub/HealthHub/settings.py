7d87f548ba64a77362457fde91d3848f669d44df
Jeon_minGyu <coby4313@gmail.com>
2022-08-11 21:32:53 +0900

    배포를 위한 mysettings 제거

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from pickle import TRUE
-import mysettings
+# import mysettings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = mysettings.KEY
+# SECRET_KEY = mysettings.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'HealthHub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = mysettings.DATABASES
+# DATABASES = mysettings.DATABASES
 
 
 
