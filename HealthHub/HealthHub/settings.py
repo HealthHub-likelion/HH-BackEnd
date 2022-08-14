5063f743043a756a7f6ca716131b9fb202ce7476
youngmin1006 <beanpole_1006@naver.com>
2022-08-14 17:49:45 +0900

    make .env file

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from pickle import TRUE
+from dotenv import load_dotenv
 # import mysettings
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
