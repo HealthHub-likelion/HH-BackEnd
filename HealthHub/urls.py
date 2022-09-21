686aad015bb3076dfd386136628e33cedfa37e2f
youngmin1006 <beanpole_1006@naver.com>
2022-09-21 14:14:09 +0900

    swagger

--- HealthHub/urls.py
+++ HealthHub/urls.py
@@ -17,8 +17,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
+
+
+
+#스웨거 적용 내용
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Health Hub", # 타이틀
+        default_version='v1', # 버전
+        description="Health Hub 최초 배포", # 설명
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    validators=['flex'],
+    public=True,
+    permission_classes=(AllowAny,)
+)
 
 urlpatterns = [
+    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
     path('admin/', admin.site.urls),
     path('exercise/',include('exercise.urls')),
     path('accounts/',include('accounts.urls')),
